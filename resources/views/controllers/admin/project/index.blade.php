d127c4360ca53b0ee2a8922c1bc6a1583b9972b2
Cody Gibbs <cgibbs@broco.com>
2015-12-07 23:20:40 -0600

    Add admin styles

--- resources/views/controllers/admin/project/index.blade.php
+++ resources/views/controllers/admin/project/index.blade.php
@@ -1,5 +1,17 @@
 @extends('layouts.admin.main')
 
 @section('content')
-    <h1>Admin</h1>
+    <div class="top-bar">
+        <div class="top-bar-left">
+            <ul class="dropdown menu" data-dropdown-menu>
+                <li class="menu-text">Site Title</li>
+                <li><a href="#">Two</a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="row">
+        <div class="small-12 columns">
+            <h1>Admin</h1>
+        </div>
+    </div>
 @endsection
--- resources/views/layouts/admin/main.blade.php
+++ resources/views/layouts/admin/main.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Awesome Code, Great Job!</title>
+    <title>Awesome Code, Great Job! | Admin</title>
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
@@ -28,7 +28,10 @@
     @yield('content')
 
 
-    <scripts src="{{ asset('js/all.js') }}"></scripts>
+    <script src="{{ asset('js/all.js') }}"></script>
+    <script>
+        $(document).foundation();
+    </script>
 
 </body>
 </html>
